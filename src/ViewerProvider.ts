a3534eb8f2b9fb33efd59701e0f53b2fb16a3109
Jason Chown <git@jasonchown.com>
2024-05-23 23:06:38 +0100

    Move out spinner

--- src/ViewerProvider.ts
+++ src/ViewerProvider.ts
@@ -1,5 +1,7 @@
 import * as vscode from 'vscode';
-import { readFile } from 'fs';
+import * as path from 'path';
+import * as fs from 'fs';
+
 import { YellowSnowView } from './YellowSnowView';
 import { GitHistory } from './GitHistory';
 import { LineFile } from './LineFile';
@@ -16,7 +18,13 @@ export class ViewerProvider implements vscode.CustomReadonlyEditorProvider<Yello
 
 	public static readonly viewType = 'yellowSnow.viewType';
 
+	spinner: string;
+
 	constructor(private readonly context: vscode.ExtensionContext) {
+
+		const htmlFilePath = vscode.Uri.file(path.join(context.extensionPath, 'spinner.html'));
+
+		this.spinner = fs.readFileSync(htmlFilePath.fsPath, 'utf8');
 	}
 
 	openCustomDocument(uri: vscode.Uri, openContext: vscode.CustomDocumentOpenContext, token: vscode.CancellationToken): vscode.CustomDocument | Thenable<vscode.CustomDocument> {
@@ -33,7 +41,7 @@ export class ViewerProvider implements vscode.CustomReadonlyEditorProvider<Yello
 		try {
 			const gitHistory = new GitHistory(document.uri.fsPath);
 
-			readFile(document.uri.fsPath, 'utf8', (err, data) => {
+			fs.readFile(document.uri.fsPath, 'utf8', (err, data) => {
 				webviewPanel.webview.html = this.getHtmlForWebview(data.split('\n'), gitHistory, webviewPanel.webview);
 			});
 		}
@@ -56,7 +64,7 @@ export class ViewerProvider implements vscode.CustomReadonlyEditorProvider<Yello
 				.loading {
 					width: 100%;
 					height: 60px;
-					background: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' version='1.1' width='575' height='6px'%3E %3Cstyle%3E circle { animation: ball 2.5s cubic-bezier(0.000, 1.000, 1.000, 0.000) infinite; fill: %23bbb; } %23balls { animation: balls 2.5s linear infinite; } %23circle2 { animation-delay: 0.1s; } %23circle3 { animation-delay: 0.2s; } %23circle4 { animation-delay: 0.3s; } %23circle5 { animation-delay: 0.4s; } @keyframes ball { from { transform: none; } 20% { transform: none; } 80% { transform: translateX(864px); } to { transform: translateX(864px); } } @keyframes balls { from { transform: translateX(-40px); } to { transform: translateX(30px); } } %3C/style%3E %3Cg id='balls'%3E %3Ccircle class='circle' id='circle1' cx='-115' cy='3' r='3'/%3E %3Ccircle class='circle' id='circle2' cx='-130' cy='3' r='3' /%3E %3Ccircle class='circle' id='circle3' cx='-145' cy='3' r='3' /%3E %3Ccircle class='circle' id='circle4' cx='-160' cy='3' r='3' /%3E %3Ccircle class='circle' id='circle5' cx='-175' cy='3' r='3' /%3E %3C/g%3E %3C/svg%3E") 50% no-repeat;
+					background: ${this.spinner};
 				}
 			</style>
 		</head>
